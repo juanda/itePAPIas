171ebbf1a8c1ecc832ed483baa97aae37d54208b
Juanda <juanda@macbook-pro-de-cnice-2.local>
2011-08-26 20:19:13 +0200

    License

--- src/TeyDe/Papi/Core/PAPIAS.php
+++ src/TeyDe/Papi/Core/PAPIAS.php
@@ -1,26 +1,18 @@
 <?php
-
 /*
- * Copyright 2010 Instituto de Tecnologías Educativas - Ministerio de Educación de España
- *
- * Licencia con arreglo a la EUPL, Versión 1.1 exclusivamente
- * (la «Licencia»);
- * Solo podrá usarse esta obra si se respeta la Licencia.
- * Puede obtenerse una copia de la Licencia en:
- *
- * http://ec.europa.eu/idabc/eupl5
- *
- * y también en:
+  This file is part of itePAPIas.
+  Foobar is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
- * http://ec.europa.eu/idabc/en/document/7774.html
- *
- * Salvo cuando lo exija la legislación aplicable o se acuerde
- * por escrito, el programa distribuido con arreglo a la
- * Licencia se distribuye «TAL CUAL»,
- * SIN GARANTÍAS NI CONDICIONES DE NINGÚN TIPO, ni expresas
- * ni implícitas.
- * Véase la Licencia en el idioma concreto que rige
- * los permisos y limitaciones que establece la Licencia.
+  Foobar is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 ?>
 <?php
